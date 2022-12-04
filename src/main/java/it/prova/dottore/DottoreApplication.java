5acfa2c582fea1ca40755359bae2ef5e2d18a6bb
MattiaLR95 <laroccamattia@virgilio.it>
2022-12-04 18:22:31 +0100

    Inserimento di due dottori campione e completamento del progetto

--- src/main/java/it/prova/dottore/DottoreApplication.java
+++ src/main/java/it/prova/dottore/DottoreApplication.java
@@ -1,13 +1,29 @@
 package it.prova.dottore;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DottoreApplication {
+import it.prova.dottore.model.Dottore;
+import it.prova.dottore.service.DottoreService;
 
+@SpringBootApplication
+public class DottoreApplication implements CommandLineRunner {
+	@Autowired
+	private DottoreService dottoreService;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DottoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		dottoreService.inserisciNuovo(new Dottore("gianluca","provola","GianlucaProvola1"));
+		
+		dottoreService.inserisciNuovo(new Dottore("Pippo","Baudo","PippoBaudo1"));
 	}
 
 }

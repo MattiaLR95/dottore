96a47ffe70533b902116bce3bef94572edec981a
MattiaLR95 <laroccamattia@virgilio.it>
2022-12-04 18:21:29 +0100

    Implementazione delle classi DTO e del Controller

---
+++ src/main/java/it/prova/dottore/dto/DottorePazienteDTO.java
@@ -0,0 +1,41 @@
+package it.prova.dottore.dto;
+
+import it.prova.dottore.model.Dottore;
+
+public class DottorePazienteDTO {
+	private String codiceDottore;
+	private String codFiscalePazienteAttualmenteInVisita;
+
+	public DottorePazienteDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DottorePazienteDTO(String codiceDottore, String codFiscalePazienteAttualmenteInVisita) {
+		super();
+		this.codiceDottore = codiceDottore;
+		this.codFiscalePazienteAttualmenteInVisita = codFiscalePazienteAttualmenteInVisita;
+	}
+
+	public String getCodiceDottore() {
+		return codiceDottore;
+	}
+
+	public void setCodiceDottore(String codiceDottore) {
+		this.codiceDottore = codiceDottore;
+	}
+
+	public String getCodFiscalePazienteAttualmenteInVisita() {
+		return codFiscalePazienteAttualmenteInVisita;
+	}
+
+	public void setCodFiscalePazienteAttualmenteInVisita(String codFiscalePazienteAttualmenteInVisita) {
+		this.codFiscalePazienteAttualmenteInVisita = codFiscalePazienteAttualmenteInVisita;
+	}
+
+	public static DottorePazienteDTO buildDottoreDTOFromModel(Dottore dottoreModel) {
+		DottorePazienteDTO result = new DottorePazienteDTO(dottoreModel.getCodiceDottore(),
+				dottoreModel.getCodFiscalePazienteAttualmenteInVisita());
+		return result;
+	}
+
+}
---
+++ src/main/java/it/prova/dottore/web/api/exception/DottoreNotFoundException.java
@@ -0,0 +1,12 @@
+package it.prova.dottore.web.api.exception;
+
+public class DottoreNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DottoreNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
